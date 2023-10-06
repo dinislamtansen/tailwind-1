e6e90a0b7f74ea9c2cc8035a76b648ddca763092
tansen2425 <dinislamtansen11@gmail.com>
2023-10-06 12:18:36 +0600

    tailwind-1

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      colors: {
+        'tans-blue': '#253042',
+        'tans-text': '#133063',
+
+      },
+    },
+  },
+  plugins: [],
+}
+
